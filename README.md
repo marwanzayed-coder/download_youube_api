3d1cd762c6d5fcbe2df27139d671de7a950547b3
marwanzayed-coder <marwanzayedshaban@gmail.com>
2023-03-09 03:12:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# download_youube_api
---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var ytdl = require("ytdl-core");
+
+app.listen("4000", function () {
+  console.log("listening on 4000");
+});
+
+app.get("/download", function (req, res) {
+  const link = req.query.url;
+  const format = req.query.format;
+  const quality = req.query.quality;
+
+  const video = ytdl(link, {
+    format: format,
+    quality: quality,
+    filter: "audioandvideo",
+  });
+  video.pipe(res);
+});
